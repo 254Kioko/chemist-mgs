85173b905d1781a09656fdcf9ccdd4c350c76384
254Kioko <128481140+254Kioko@users.noreply.github.com>
2025-10-10 16:34:19 +0300

    Update Auth.tsx

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -103,7 +103,7 @@ export default function Auth() {
               {loading ? 'Signing in...' : 'Sign In'}
             </Button>
             <div className="text-xs text-center text-muted-foreground mt-4">
-              <p> <strong>contact developer incase</strong> |  <strong>of any inquiry</strong></p>
+              <p> <strong>contact developer incase</strong> <strong>of any inquiry</strong></p>
             </div>
           </form>
         </CardContent>
