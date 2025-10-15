6e314c5b565cfcbb904e8ccfe92d0b3b24ea4b2e
254Kioko <128481140+254Kioko@users.noreply.github.com>
2025-10-15 22:33:07 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Auth from "./pages/Auth";
 import Manager from "./pages/Manager";
-import Supplier from "./pages/Supplier";
-
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -22,7 +20,6 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/manager" element={<Manager />} />
-          <Route path="/supplier" element={<Supplier />} />
 
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
