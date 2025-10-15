11914acf2c219d24f34b1b177d1a0b6938f890f6
254Kioko <128481140+254Kioko@users.noreply.github.com>
2025-10-15 22:18:25 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Auth from "./pages/Auth";
 import Manager from "./pages/Manager";
+import Supplier from "./pages/Supplier";
 
 import NotFound from "./pages/NotFound";
 
